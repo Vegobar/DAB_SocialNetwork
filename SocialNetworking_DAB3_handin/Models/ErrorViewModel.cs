62b4faaa971e614a1b5a849a7494086220c28f47
Mikkel Schmidt Andersen <mikkel220897@hotmail.com>
2020-04-22 12:56:34 +0200

    Removed old console app with asp.net mvc

---
+++ SocialNetworking_DAB3_handin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SocialNetworking_DAB3_handin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
